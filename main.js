65e277ddfb9a65dc4eb7409d362d816f6b99fbb9
Brandon McGhee <brandonmcghee@Brandons-MacBook-Pro.local>
2012-06-29 20:24:57 -0400

    Created main.js file to house my Javascript

---
+++ main.js
@@ -0,0 +1,19 @@
+//Brandon McGhee
+//Assignment 2
+//VFW
+
+var saveData = function() {
+    
+}
+
+var getData = function() {
+    
+}
+
+var clearData = function() {
+    
+}
+
+var arrayReplace = function() {
+    
+}
