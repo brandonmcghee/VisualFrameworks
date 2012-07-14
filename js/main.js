db4436b0c146485b66ffaea473c9e45a30189395
Brandon McGhee <brandonmcghee@Brandons-MacBook-Pro.local>
2012-07-14 18:44:12 -0400

    Made all changes from the videos

--- js/main.js
+++ js/main.js
@@ -10,6 +10,76 @@ window.addEventListener("DOMContentLoaded", function () {
         return theElement;
     }
     
+    //Validation
+    function validate(e) {
+        
+        var getSpirit = $('spiritName');
+        var getBottle = $('slideVAL');
+        var getShelve = $('topShelve');
+        var getFamily = $('spiritFamily');
+        var getDate = $('datePurchase');
+        
+        var messageAry = [];
+        
+        //Reset error message
+        errMsg.innerHTML = "";
+            getSpirit.style.border = "1px solid black";
+            getBottle.style.border = "1px solid black";
+            getShelve.style.border = "1px solid black";
+            getFamily.style.border = "1px solid black";
+            getDate.style.border = "1px solid black";
+        
+        //Name Validation
+        if (getSpirit.value == "") {
+            var nameError = "Please enter the Spirit's Name";
+            getSpirit.style.border = "2px solid red";
+            messageAry.push(nameError);
+        }
+        
+        //Bottle Size Validation
+        if (getBottle.value == "") {
+            var bottleError = "Please select a size of the bottle";
+            getBottle.style.border = "2px solid red";
+            messageAry.push(bottleError);
+        }
+        
+        //Shelve Quality Validation
+        if (getShelve.value == "") {
+            var shelveError = "Please select a shelve quality";
+            $('topShelve').style.border = "1px solid red";
+            messageAry.push(shelveError);
+        }
+        
+        //Family Validation
+        if (getFamily.value == "---Select Spirit Family---") {
+            var familyError = "Please select a spirit family";
+            getFamily.style.border = "2px solid red";
+            messageAry.push(familyError);
+        }
+        
+        //Date Purchased Validation
+        if (getDate.value == "") {
+            var dateError = "Please select a purchase date";
+            getDate.style.border = "2px solid red";
+            messageAry.push(dateError);
+        }
+        
+        //Display Errors, if any
+        if (messageAry.length >= 1) {
+            for (var i = 0, j = messageAry.length; i < j; i++) {
+                var txt = document.createElement('li');
+                txt.innerHTML = messageAry[i];
+                errMsg.appendChild(txt);
+            }
+            e.preventDefault();
+            return false;
+        }else{
+            //If its all valid
+            storeData(this.key);  
+        }
+    
+    }
+    
     //Constructs the family select element filled with Family Spirits
     function buildFamily() {
         var formTag = document.getElementsByTagName("form"),
@@ -59,23 +129,32 @@ window.addEventListener("DOMContentLoaded", function () {
     }
     
     //Stores data into Local Storage
-    function storeData() {
-        var id              = Math.floor(Math.random()*100000001);
+    function storeData(key) {
+        //If there is no key, this means this is a brand new item and we need a new key.
+        if (!key) {
+            var id              = Math.floor(Math.random()*100000001);
+        }else{
+            //Set the id to the existing key we're editing so that it will save over the data
+            //The key is the same key that's been passed along from the editSubmit event handler
+            //to the validate function, and then passed here, into the storeData function.
+            id = key;
+        }
+
         //Gather all form field values and store in an object.
         //Object properties contain array with the form label and input value.
         
         //Caling Radio Function to see which one the user chose
         getSelectedRadio();
         //Stores form data into an object
-        var item            = {};
-            item.spiritName = ["Name: ", $('spiritName').value];
-            item.bottleMIL  = ["Bottle Size: ", $('bottleMIL').value + " ML"];
-            item.shelve     = ["Quality: ", shelveValue];
-            item.family     = ["Family: ", $('spiritFamily').value];
-            item.date       = ["Date Purchased: ", $('datePurchase').value];
+        var spirit            = {};
+            spirit.spiritName = ["Name: ", $('spiritName').value];
+            spirit.bottleMIL  = ["Bottle Size: ", $('slideVAL').value];
+            spirit.shelve     = ["Quality: ", shelveValue];
+            spirit.family     = ["Family: ", $('spiritFamily').value];
+            spirit.date       = ["Date Purchased: ", $('datePurchase').value];
             
         //Save into local storage: Use stringify to convert object to a string.
-        localStorage.setItem(id, JSON.stringify(item));
+        localStorage.setItem(id, JSON.stringify(spirit));
         alert("Spirit Stored!");
         toggleControls("on");
     }
@@ -98,6 +177,7 @@ window.addEventListener("DOMContentLoaded", function () {
         $('items').style.display = "block";
         for (var i = 0, len=localStorage.length; i < len; i++) {
             var makeli = document.createElement('li');
+            var linksLi = document.createElement('li');
             makeList.appendChild(makeli);
             var key = localStorage.key(i);
             var value = localStorage.getItem(key);
@@ -110,7 +190,82 @@ window.addEventListener("DOMContentLoaded", function () {
                 makeSubList.appendChild(makeSubli);
                 var optSubText = obj [n] [0] + "  " + obj [n] [1];
                 makeSubli.innerHTML = optSubText;
+                makeSubList.appendChild(linksLi);
             }
+            makeItemLinks(localStorage.key(i), linksLi);
+        }
+    }
+    
+    function makeItemLinks(key, linksLi) {
+        var breakTag = document.createElement('br');
+        linksLi.appendChild(breakTag);
+        var editLink = document.createElement('a');
+        editLink.href = "#";
+        editLink.key = key;
+        editLink.className = "edit";
+        var editText = "Edit Spirit";
+        editLink.addEventListener("click", editSpirit);
+        editLink.innerHTML = editText;
+        linksLi.appendChild(editLink);
+        
+        var deleteLink = document.createElement('a');
+        deleteLink.href = "#";
+        deleteLink.key = key;
+        deleteLink.className = "delete";
+        var deleteText = "Delete Spirit";
+        deleteLink.addEventListener("click", deleteSpirit);
+        deleteLink.innerHTML = deleteText;
+        linksLi.appendChild(deleteLink);
+        
+        linksLi.appendChild(breakTag);
+        
+    }
+    
+    function editSpirit() {
+        
+        var value = localStorage.getItem(this.key);
+        var spirit = JSON.parse(value);
+        var radios = document.forms[0].shelve;
+        
+        toggleControls("off");
+        
+        $('spiritName').value = spirit.spiritName[1];
+        $('slideVAL').value = spirit.bottleMIL[1];
+        $('spiritFamily').value = spirit.family[1];
+        $('datePurchase').value = spirit.date[1];
+        
+        for (var i = 0; i < radios.length; i++) {
+            if (radios[i].value == "Top Shelve" && spirit.shelve[1] == "Top Shelve") {
+                radios[i].setAttribute("checked", "checked");
+            }else if (radios[i].value == "Middle Shelve" && spirit.shelve[1] == "Middle Shelve") {
+                radios[i].setAttribute("checked", "checked");
+            }else if (radios[i].value == "Bottom Shelve" && spirit.shelve[1] == "Bottom Shelve") {
+                radios[i].setAttribute("checked", "checked");
+            }else{
+                //need something here!
+            }
+            
+            
+        }
+        
+        //Remove the initial listener from the input Store Spirit button
+        save.removeEventListener("click", storeData);
+        
+        //Change value of submit button to edit
+        $('submit').value = "Edit Spirit >>";
+        var editSubmit = $('submit');
+        editSubmit.addEventListener("click", validate);
+        editSubmit.key = this.key;
+    }
+    
+    function deleteSpirit() {
+        var ask = confirm("Are you sure you want to delete this spirit from your inventory?");
+        if (ask) {
+            localStorage.removeItem(this.key);
+            alert("Spirt has been removed");
+            window.location.reload();
+        }else{
+            alert("Spirt was NOT removed.");
         }
     }
     
@@ -133,7 +288,6 @@ window.addEventListener("DOMContentLoaded", function () {
     
     //Gathers current value of Slider and displays it to a text field
     function slider() {
-        var mil = ["375 ml", "750 ml", "1.5 L", "3000 L", "4500 L"];
         var slide = $('bottleMIL').value;
         
         //Changes the value of the "Size of Bottle" text box to reflect accurate measurements
@@ -147,6 +301,7 @@ window.addEventListener("DOMContentLoaded", function () {
     
     //Var defaults
     var shelveValue;
+    var errMsg = $('errors');
     var spiritFamily = [
                         "---Select Spirit Family---",
                         "Whiskey",
@@ -158,14 +313,19 @@ window.addEventListener("DOMContentLoaded", function () {
                         "Brandy",
                         "Vermouth",
                         "Sake"];
+    var mil = [
+                "375 ml",
+                "750 ml",
+                "1.5 L",
+                "3 L",
+                "4.5 L"];
     
     //Calling buildFamily to construct drop down menu
     buildFamily();
     
-
     //Set Store Spirit, Display Spirits, Clear Spirits and Slider Click Events
     var save = $('submit');
-    save.addEventListener("click", storeData);
+    save.addEventListener("click", validate);
     
     var display = $('display');
     display.addEventListener("click", getData);
